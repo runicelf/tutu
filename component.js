5a013b7f31f157e3e0a54f1a6cb28524e3947fe2
runicelf <perovrsq@gmail.com>
2017-04-08 23:11:24 +0300

    added onload indicator

--- component.js
+++ component.js
@@ -18,7 +18,7 @@ function httpGet(url) {
 }
 
 const url = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}';
-const url2  = 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}';
+const url2 = 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}';
 const table = document.getElementById('table-ins');
 const searchButton = document.getElementById('search-button');
 const additionalInfo = document.getElementById('additional-info');
